e336a7c3c9399fde23cd637103fff29617da5a26
jack8096 <jackdeepakkamble@gmail.com>
2023-09-07 14:14:21 +0530

    added dynamic appbar

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,5 @@
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
-import 'package:flutter/cupertino.dart';
+//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gdsc/pages/eventpage.dart';
 import 'package:gdsc/pages/teampage.dart';
@@ -10,6 +10,8 @@ import 'about_us.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -36,16 +38,14 @@ class _MyHomePageState extends State<MyHomePage> {
     const EventPage(),
     const AboutPage(),
   ];
-
+int appbarColorIndex = 0;
   @override
   Widget build(BuildContext context) {
+    final List appbarColor = <Color>[const Color.fromARGB(255,242,139,130),const Color.fromARGB(255,138,180,248,) , const Color.fromARGB(255, 129,201,149),];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: 
-        const Color.fromARGB(255, 129,201,149),
-        //shadowColor: ,
-        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        //title: Text(widget.title),
+        appbarColor[appbarColorIndex],
         actions: [
           IconButton(onPressed: (){}, icon: const Icon(Ionicons.search)), 
           IconButton(onPressed: (){}, icon: const Icon(Ionicons.person_circle))
@@ -61,6 +61,10 @@ class _MyHomePageState extends State<MyHomePage> {
         currentIndex:
             _controller.index, // Pass the current index from your controller
         onTap: (index) {
+        setState(() {                
+      appbarColorIndex = index;
+    });
+          
           _controller.index = index; // Update the controller index
           _pageController.jumpToPage(index); // Jump to the selected page
         },
